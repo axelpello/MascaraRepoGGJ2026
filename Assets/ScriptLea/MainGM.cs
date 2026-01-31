1546a0ab017808941b72f9f097ac6cc7bc6a767c
axelpello <axelpello@hotmail.com>
2026-01-31 15:37:01 -0300

    e

--- Assets/ScriptLea/MainGM.cs
+++ Assets/ScriptLea/MainGM.cs
@@ -1,9 +1,11 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MainGM : MonoBehaviour
 {
     [SerializeField]
     GameObject[] MiniJuegos = new GameObject[3];
+    public GameObject selectorLevel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,19 +26,23 @@ public class MainGM : MonoBehaviour
                     switch (hit.collider.tag)
                     {
                         case "0":
-                            Instantiate(MiniJuegos[0], new Vector3(0, 0, 0), Quaternion.identity);
-                            Debug.Log("0");
+                            MiniJuegos[0].transform.gameObject.SetActive(true);
+                            //Instantiate(MiniJuegos[0], new Vector3(0, 0, 0), Quaternion.identity);
+                            Debug.Log("Juego 0 activado");
                             break;
                         case "1":
-                            Instantiate(MiniJuegos[1], new Vector3(0, 0, 0), Quaternion.identity);
-                            Debug.Log("1");
+                            MiniJuegos[1].transform.gameObject.SetActive(true);
+                            Debug.Log(MiniJuegos[1].transform.gameObject.activeSelf);
+                            //Instantiate(MiniJuegos[1], new Vector3(0, 0, 0), Quaternion.identity);
+                            Debug.Log("Juego 1 haktibado");
                             break;
                         case "2":
-                            Instantiate(MiniJuegos[2], new Vector3(0, 0, 0), Quaternion.identity);
-                            Debug.Log("2");
+                            MiniJuegos[2].transform.gameObject.SetActive(true);
+                            //Instantiate(MiniJuegos[2], new Vector3(0, 0, 0), Quaternion.identity);
+                            Debug.Log("Juego 2 activado");
                             break;
-
                     }
+                    selectorLevel.transform.gameObject.SetActive(false);
                 }
                 //Debug.Log("Click sobre: " + hit.collider.name);
             }
